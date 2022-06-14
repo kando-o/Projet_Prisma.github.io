b87b0deef3cb9259278373e811abee5a087b3f40
Jason Kando <kando.jason@live.fr>
2022-06-14 16:26:50 +0200

    Responsive Menu Ok

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 let hamburger = document.querySelector('.navHamburger');
-let container = document.querySelector('.container')
+let menuHidden = document.querySelector('.bgMenuHidden')
+let barre1 = document.querySelector('.b1')
+let barre2 = document.querySelector('.b2')
+let barre3 = document.querySelector('.b3')
+
 console.log(hamburger);
 
 hamburger.addEventListener('click', () => {
-    container.classList.toggle('hiddenContainer')
+    menuHidden.classList.toggle("hidden") ;
+    barre1.classList.toggle('translateb1');
+    barre2.classList.toggle('rotateb2');
+    barre3.classList.toggle('translateb3');
+
 })
