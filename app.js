d75e88c7556e6b4e880dbaf04f25193d6f1ec1be
Jason Kando <kando.jason@live.fr>
2022-06-15 16:09:48 +0200

    Retouche Parallaxe

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 let hamburger = document.querySelector('.navHamburger');
 let menuHidden = document.querySelector('.bgMenuHidden')
+
+let banner = document.querySelector('.banner')
+let bannerImg = document.querySelector('img')
+
+
 let barre1 = document.querySelector('.b1')
 let barre2 = document.querySelector('.b2')
 let barre3 = document.querySelector('.b3')
 
-console.log(hamburger);
+// Event au click sur l'icone hamburger
 
 hamburger.addEventListener('click', () => {
     menuHidden.classList.toggle("hidden") ;
@@ -12,4 +17,11 @@ hamburger.addEventListener('click', () => {
     barre2.classList.toggle('rotateb2');
     barre3.classList.toggle('translateb3');
 
+})
+
+// Parallaxe 
+
+window.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+    banner.style.backgroundPositionY = window.scrollY / 2 + "px" ;
 })
